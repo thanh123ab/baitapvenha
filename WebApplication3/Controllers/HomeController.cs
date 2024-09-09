48284b1c9e6bc435372c45a531d7ed245639296c
Cao Đức Thành <caoducthanh18@gmail.com>
2024-09-09 21:58:12 +0700

    a

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -1,35 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using WebApplication3.Models;
-using lib_baitoan;
+using lib_baitoan;  // Sử dụng thư viện bạn đã tạo
 
-namespace WebApplication3.Controllers
+namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
-
-
-private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        // Phương thức để render trang index
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        // Phương thức để xử lý tính toán và trả kết quả
+        [HttpPost]
+        public JsonResult TinhDienTich([FromBody] HinhNonInput input)
         {
-            return View();
-        }
+            if (input.Radius <= 0 || input.Height <= 0)
+            {
+                return Json(new { error = "Bán kính và chiều cao phải lớn hơn 0." });
+            }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            double l = Class1.TinhDuongSinh(input.Radius, input.Height);
+            double dienTichDay = Class1.TinhDienTichDay(input.Radius);
+            double dienTichXungQuanh = Class1.TinhDienTichXungQuanh(input.Radius, l);
+            double dienTichToanPhan = Class1.TinhDienTichToanPhan(input.Radius, input.Height);
+
+            return Json(new
+            {
+                l = l.ToString("0.00"),
+                dienTichDay = dienTichDay.ToString("0.00"),
+                dienTichXungQuanh = dienTichXungQuanh.ToString("0.00"),
+                dienTichToanPhan = dienTichToanPhan.ToString("0.00")
+            });
         }
+    }
+
+    public class HinhNonInput
+    {
+        public double Radius { get; set; }
+        public double Height { get; set; }
     }
 }
